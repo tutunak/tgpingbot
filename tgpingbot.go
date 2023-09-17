9b0fbaf07225119a4685bc46c63917d77c45c57e
tutunak <dk@tutunak.com>
2023-09-17 23:12:10 +0400

    change the logger

--- tgpingbot.go
+++ tgpingbot.go
@@ -1,11 +1,29 @@
 package main
 
 import (
+	"encoding/json"
 	tele "gopkg.in/telebot.v3"
 	"log"
 	"os"
 	"time"
 )
+
+func customLogger(logger ...*log.Logger) tele.MiddlewareFunc {
+	var l *log.Logger
+	if len(logger) > 0 {
+		l = logger[0]
+	} else {
+		l = log.Default()
+	}
+
+	return func(next tele.HandlerFunc) tele.HandlerFunc {
+		return func(c tele.Context) error {
+			data, _ := json.Marshal(c.Update())
+			l.Println(string(data))
+			return next(c)
+		}
+	}
+}
 
 func setUpHandlers(bot *tele.Bot) {
 	bot.Handle("/ping", func(context tele.Context) error {
@@ -29,7 +47,7 @@ func main() {
 		log.Fatal(err)
 		return
 	}
-
+	bot.Use(customLogger())
 	setUpHandlers(bot)
 	bot.Start()
 }
